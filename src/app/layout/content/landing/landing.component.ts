2a5316507e6a6334db5f626b73a199585558c64d
CoderTofu <vioneverforget@gmail.com>
2024-07-19 14:42:51 +0800

    minor visual update on landing page

--- src/app/layout/content/landing/landing.component.ts
+++ src/app/layout/content/landing/landing.component.ts
@@ -9,4 +9,39 @@ import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
-export class LandingComponent {}
+export class LandingComponent {
+  landingHeader: HTMLElement | null = null;
+  landingSpan: HTMLElement | null = null;
+  searchBar: HTMLElement | null = null;
+  editBar: HTMLElement | null = null;
+  createBar: HTMLElement | null = null;
+
+  toggleSearchBar() {
+    this.landingHeader = document.getElementById('landing-header');
+    this.landingSpan = this.landingHeader?.querySelector('span') as HTMLElement;
+    this.searchBar = document.getElementById('search_bar');
+
+    this.searchBar?.focus();
+    this.landingSpan.style.color = '#f6cd35';
+  }
+
+  toggleEditBar() {
+    this.editBar = document.getElementById('edit_bar');
+    this.landingSpan = document
+      .getElementById('landing-header')
+      ?.querySelector('span') as HTMLElement;
+
+    this.editBar?.focus();
+    this.landingSpan.style.color = '#d13775';
+  }
+
+  toggleCreateBar() {
+    this.createBar = document.getElementById('create_bar');
+    this.landingSpan = document
+      .getElementById('landing-header')
+      ?.querySelector('span') as HTMLElement;
+
+    this.createBar?.focus();
+    this.landingSpan.style.color = '#26ace1';
+  }
+}
